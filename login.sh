23b602ea06c5aabc4d6866ed6ba429c6b5692997
sesaiqbal-code <sesaiqbal@gmail.com>
2026-02-21 01:11:56 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,5 +1,35 @@
 #!/system/bin/sh
 
-input text email@gmail.com
+ID=$(settings get secure android_id)
+
+EMAIL_LIST="
+gmail1@gmail.com
+gmail2@gmail.com
+gmail3@gmail.com
+gmail4@gmail.com
+gmail5@gmail.com
+gmail6@gmail.com
+gmail7@gmail.com
+gmail8@gmail.com
+gmail9@gmail.com
+gmail10@gmail.com
+gmail11@gmail.com
+gmail12@gmail.com
+gmail13@gmail.com
+gmail14@gmail.com
+gmail15@gmail.com
+gmail16@gmail.com
+gmail17@gmail.com
+gmail18@gmail.com
+gmail19@gmail.com
+gmail20@gmail.com
+"
+
+INDEX=$(echo $ID | tail -c 2)
+NUM=$((0x$INDEX % 20 + 1))
+
+EMAIL=$(echo "$EMAIL_LIST" | sed -n "${NUM}p")
+
+input text $EMAIL
 sleep 1
 input keyevent 66
