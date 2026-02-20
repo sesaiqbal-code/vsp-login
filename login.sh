de76919b0474f2fcff2ba840dd10e1585501d3e8
sesaiqbal-code <sesaiqbal@gmail.com>
2026-02-21 01:38:25 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,63 +1,67 @@
 #!/system/bin/sh
 
-sleep 7
+sleep 2
 
-# Ambil ID device
+BATCH=0
+
+emails=(
+delhi1%40asetsaya.com
+delhi2%40asetsaya.com
+delhi3%40asetsaya.com
+delhi4%40asetsaya.com
+delhi5%40asetsaya.com
+delhi6%40asetsaya.com
+delhi7%40asetsaya.com
+delhi8%40asetsaya.com
+delhi9%40asetsaya.com
+delhi10%40asetsaya.com
+delhi11%40asetsaya.com
+delhi12%40asetsaya.com
+delhi13%40asetsaya.com
+delhi14%40asetsaya.com
+delhi15%40asetsaya.com
+delhi16%40asetsaya.com
+delhi17%40asetsaya.com
+delhi18%40asetsaya.com
+delhi19%40asetsaya.com
+delhi20%40asetsaya.com
+delhi21%40asetsaya.com
+delhi22%40asetsaya.com
+delhi23%40asetsaya.com
+delhi24%40asetsaya.com
+delhi25%40asetsaya.com
+delhi26%40asetsaya.com
+delhi27%40asetsaya.com
+delhi28%40asetsaya.com
+delhi29%40asetsaya.com
+delhi30%40asetsaya.com
+delhi31%40asetsaya.com
+delhi32%40asetsaya.com
+delhi33%40asetsaya.com
+delhi34%40asetsaya.com
+delhi35%40asetsaya.com
+delhi36%40asetsaya.com
+delhi37%40asetsaya.com
+delhi38%40asetsaya.com
+delhi39%40asetsaya.com
+delhi40%40asetsaya.com
+)
+
 device_id=$(getprop ro.boot.serialno)
 if [ -z "$device_id" ]; then
 device_id=$(settings get secure android_id)
 fi
 
-echo "Device: $device_id"
-
 num=$(echo $device_id | tr -cd '0-9')
 index=$(echo $num | tail -c 2)
 index=$((index % 20))
 
-emails=(
-gmail1%40gmail.com
-gmail2%40gmail.com
-gmail3%40gmail.com
-gmail4%40gmail.com
-gmail5%40gmail.com
-gmail6%40gmail.com
-gmail7%40gmail.com
-gmail8%40gmail.com
-gmail9%40gmail.com
-gmail10%40gmail.com
-gmail11%40gmail.com
-gmail12%40gmail.com
-gmail13%40gmail.com
-gmail14%40gmail.com
-gmail15%40gmail.com
-gmail16%40gmail.com
-gmail17%40gmail.com
-gmail18%40gmail.com
-gmail19%40gmail.com
-gmail20%40gmail.com
-)
+real_index=$((BATCH * 20 + index))
 
-email=${emails[$index]}
+email=${emails[$real_index]}
 
 echo "Email dipakai: $email"
 
-# Fokus ke kolom Gmail (beberapa posisi supaya pasti kena)
-input tap 360 420
-sleep 1
-input tap 360 460
-sleep 1
-input tap 360 500
-sleep 1
-
-# Isi email
 input text $email
-sleep 2
-
-# Tekan Next dari keyboard
-input keyevent 66
-sleep 2
-
-# Klik tombol Next juga (backup)
-input tap 600 820
 sleep 1
-input tap 600 820
+input keyevent 66
