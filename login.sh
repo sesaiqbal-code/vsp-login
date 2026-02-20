a6e5a964105fd6cb46f697a7e9b5eed91c695153
sesaiqbal-code <sesaiqbal@gmail.com>
2026-02-21 02:06:04 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -3,15 +3,19 @@
 sleep 2
 
 BATCH=1
+TOTAL=20
 
 device=$(settings get secure android_id)
-num=$(echo $device | tr -cd '0-9')
-slot=$(echo $num | tail -c 2)
-slot=$((slot % 20))
 
-start=$(( (BATCH - 1) * 20 + 1 ))
+num=$(echo $device | tr -cd '0-9')
+slot=$(echo $num | tail -c 3)
+slot=$((slot % TOTAL))
+
+start=$(( (BATCH - 1) * TOTAL + 1 ))
 email_number=$(( start + slot ))
 
+echo "Device: $device"
+echo "Slot: $slot"
 echo "Email dipakai: delhi$email_number@asetsaya.com"
 
 input text "delhi$email_number"
