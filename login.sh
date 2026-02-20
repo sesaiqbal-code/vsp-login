a51d92afe90c7939965717b09f9aed7092a345ab
sesaiqbal-code <sesaiqbal@gmail.com>
2026-02-21 01:55:49 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,53 +2,19 @@
 
 sleep 2
 
-emails="
-delhi1
-delhi2
-delhi3
-delhi4
-delhi5
-delhi6
-delhi7
-delhi8
-delhi9
-delhi10
-delhi11
-delhi12
-delhi13
-delhi14
-delhi15
-delhi16
-delhi17
-delhi18
-delhi19
-delhi20
-delhi21
-delhi22
-delhi23
-delhi24
-delhi25
-delhi26
-delhi27
-delhi28
-delhi29
-delhi30
-"
+BATCH=1   # ganti 1,2,3,4,5,6 sesuai batch
 
 device=$(settings get secure android_id)
-
 num=$(echo $device | tr -cd '0-9')
 slot=$(echo $num | tail -c 2)
 slot=$((slot % 20))
 
-index=$((slot + 1))
+start=$(( (BATCH - 1) * 20 + 1 ))
+email_number=$(( start + slot ))
 
-user=$(echo "$emails" | sed -n "${index}p")
+echo "Email dipakai: delhi$email_number@asetsaya.com"
 
-echo "User dipakai: $user"
-
-# ketik email
-input text "$user"
+input text "delhi$email_number"
 input text "@asetsaya"
 input text ".com"
 
